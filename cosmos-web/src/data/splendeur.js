356a925c340763233e79429bda35dce9ee06b762
NeousAxis <dev@neousaxis.local>
2025-12-26 18:41:38 +0700

    refactor: shorten cosmosophie section titles

--- cosmos-web/src/data/splendeur.js
+++ cosmos-web/src/data/splendeur.js
@@ -1,7 +1,7 @@
 export const COSMOSOPHIE_SECTIONS = [
     {
         id: 1,
-        title: "Depuis toujours",
+        title: "Intro",
         content: `Depuis toujours, les astres jouent un rôle de première importance dans la construction et l'évolution de l'humanité. L'être humain ne cesse de lever le regard en direction du ciel pour comprendre d'où il vient et comment se rendre à destination. Cette science est fondée sur des observations empiriques. Elle a conclu à la naissance de l'astronomie et de l'astrologie.
 
 Autrefois, ces deux sciences n'en était qu'une seule et l'observation de ces combinaisons stellaires étaient nommés présages. Avec le temps, ces phénomènes naturels ont forgé des croyances et des superstitions. Ces événements propices à la manifestation de certains phénomènes sur la nature continuent pourtant de renseigner les humains. La météorologie est basée sur ce même processus empirique et grâce à la science nous continuons d'affiner nos observations ainsi que les prévisions.
@@ -26,7 +26,7 @@ En inde et au tibet, nous utilisons encore cette grammaire stellaire. Nous limit
     },
     {
         id: 4,
-        title: "La cosmosophie est le Hatha cosmique",
+        title: "La Cosmosophie",
         content: `La cosmosophie est le Hatha cosmique, le Yoga des étoiles. Ces alignements de l'univers sont comme ceux des aiguilles d'une montre. Ils donnent une information pour aligner le monde intérieur avec les événements extérieurs et influencer ainsi le monde de façon positive. L'univers est comme une horloge mécanique qui informe quand, mais aussi pourquoi aligner toutes les dimensions du corps physique, émotionnel et mental pour soutenir et concourir à la divine floraison de la conscience. L'astrologie spirituelle révèle l'intelligence organique de tous ces mouvements cosmiques. Ces alignements sont tout sauf anodins. Ils sont le fondement de l'organisation des cycles de la vie sur la Terre et au-delà.
 
 D'aucuns diraient que l'humain ne fait que tenter de traduire ces mouvements pour se sécuriser, pour mettre du sens à cause de sa peur de mourir ou de ne pas maîtriser son environnement. C'est tout à fait vrai, nous sommes dotés de cette spécificité mentale qui a pour fonction de révéler les connexions et le sens des phénomènes qui nous traversent. Ce que nous prenons pour une nécessité exercée par la peur n'est en fait qu'une nécessité humaine. De la même façon que les pommiers ont la nécessité de produire des pommes, l'humain plonge par nécessité dans le mystère de la vie pour en révéler la splendeur. Ces connexions sont sublimes et révèlent l'immense intelligence de la Vie.
@@ -46,7 +46,7 @@ Intégration spirituelle : Lorsqu'il est question de la nouvelle lune, nous parl
     },
     {
         id: 6,
-        title: "Ainsi, le Yoga des étoiles",
+        title: "Le Yoga des étoiles",
         content: `Ainsi, le Yoga des étoiles, accomplit le cycle mais jamais il ne se termine vraiment. La fin n'est rien d'autre que l'annonce d'un nouveau départ pour une nouvelle ascension. La mort est la porte de la renaissance. La voie du Yoga consiste à placer le vivant (physique, émotionnelle, mental et spirituel) au service de la puissante énergie de Vie qui s'écoule à travers les étoiles.
 
 Alignement spirituel : Silence - Intention
@@ -58,14 +58,14 @@ Il n'existe pas de postures physiques ou asanas qui s'accorderaient avec le mouv
     },
     {
         id: 7,
-        title: "En comprenant ces correspondances",
+        title: "Trouver l'équilibre",
         content: `En comprenant ces correspondances, on saisit que les principes de l'astrologie sont inscrits dans notre anatomie. Nos jambes représentent les deux piliers sur lesquels repose l'arche de la sagesse, pour qu'elle échappe au déluge des ignorances. C'est le sens caché du récit biblique de l'empereur gravant les secrets de la connaissance autour de l'Arche dressée sur deux colonnes. Cette superposition astrologique vous fera découvrir ce qui se trouve dans les temples souterrains des différentes régions terrestres. Ces temples existent dans les replis de la matière de votre cerveau, de votre palais, de votre cage thoracique, et dans les circonvolutions de votre estomac et intestins.
 
 Nous sommes invités à ajuster en conscience notre posture de vie aussi continuellement que possible, à donner un élan plus avant à cette énergie qui nous traverse. C'est-à-dire à trouver l'équilibre entre les aspirations précieuses de l'âme orientées vers la bonté, la beauté et la vérité et les nécessités d'un monde qui souffre.`
     },
     {
         id: 8,
-        title: "Ces mouvements cosmiques agissent par cycles",
+        title: "Mouvements Cosmiques",
         content: `Ces mouvements cosmiques agissent par cycles ou révolutions, ils mettent en friction toutes les dualités que nous retrouvons dans toutes les sagesses ancestrales. Puisque tout est relié, le principe de correspondance amène ces mêmes dualités dans l'être humain. Elles renseignent sur les qualités et les ombres en chacun de nous. La connaissance de soi est la voie par laquelle la conscience humaine peut apprendre à décrypter ces messages.
 
 L'apprentissage à se rencontrer, à se connaître soutient l'évolution de la conscience universelle. Cette énergie que l'on nomme la Vie à travers l'âme, la conscience humaine, pousse inlassablement vers un accomplissement supérieur. Ce que nous définissons comme le développement personnel est la portion individuelle du développement de la conscience universelle Une et Inséparable.
@@ -79,7 +79,7 @@ Masculin - Féminin
     },
     {
         id: 9,
-        title: "L'astrologie a pour objectif ancestral d'offrir à l'humanité la possibilité de dialoguer",
+        title: "L'objectif de l'astrologie",
         content: `L'astrologie a pour objectif ancestral d'offrir à l'humanité la possibilité de dialoguer avec le cosmos et comprendre comment l'évolution de notre système solaire se joue aussi au travers de la minuscule destinée atomique de l'être humain. En saisissant, en partie, les aspirations célestes, l'humanité peut apprendre à orienter son existence vers la sublime lumière de l'âme et à enraciner ses aspirations dans une fraternité qui soulève l'ensemble des parties qui la compose vers sa réalisation. Ces douze messages sonnent comme des notes de musique, elles travaillent à harmoniser tous les opposés.
 
 Ainsi, le manque d'équilibre issu du besoin d'expériences et d'enrichissement individuels et collectifs induit de passer d'un extrême à l'autre. C'est un retour d'énergie opposé pour compenser l'énergie engagée précédemment et revenir à l'équilibre. Toutes les expériences menées sur un axe de force de la dualité doivent également être expérimentées pour compenser l'énergie et obtenir une expérience complète. Ainsi, chaque événement poursuit une ligne de rétablissement ou de guérison des événements passés. La vie, au travers des formes, poursuit un chemin de rédemption.
@@ -88,21 +88,21 @@ En effet, l'expérience complète est celle qui a été menée à tous degrés d
     },
     {
         id: 10,
-        title: "Les archétypes du féminin et du masculin dialoguent ensemble jusqu'à élever",
+        title: "Les archétypes",
         content: `Les archétypes du féminin et du masculin dialoguent ensemble jusqu'à élever cycle après cycle la conscience de la Terre et simultanément l'ensemble des consciences qui œuvrent avec et pour Elle. Cette symbiose est à l'œuvre consciemment pour certaines personnes et inconsciemment pour d'autres tout simplement car nous avançons toutes et tous selon notre niveau de conscience. Celui-ci se traduit par la façon dont nous mettons à l'œuvre nos capacités, nos besoins et aussi nos souffrances. Cependant, l'unité de la conscience est toujours à l'œuvre de manière globale. L'unité apparaît divisée dans la forme mais elle reste éternellement reliée. Même la guerre qui est la division la plus manifeste maintient pourtant les protagonistes en lien. L'unité est par conséquent, la seule réalité qu'il nous faut accepter.
 
 Nous ne cesserons jamais d'apprendre à être et à vivre ensemble. Dans cet apprentissage à grandir, la conscience individuelle humaine poursuit une coopération aimante avec la conscience universelle. Chaque individu est comme un doigt de la main de l'humanité. Une extension individualisée de la conscience universelle. Chaque doigt est unique et a ses propres capacités mais l'ampleur de sa réalisation se révèle lorsqu'il se met à contribution avec les autres doigts pour effectuer une action que seule la main entière peut comprendre. Là se situe une clé importante pour comprendre cette étreinte dualiste qui imprègne tout notre être.`
     },
     {
         id: 11,
-        title: "L'âme recherche la libération",
+        title: "La libération",
         content: `L'âme recherche la libération. Le corps recherche la satisfaction des désirs et plaisirs sensoriels. La guérison des souffrances humaines est le travail de guérison aussi appelé « spiritualisation de la matière ». Tous les êtres humains sont amenés à réaliser un jour ou l'autre ce chemin de rédemption. Cela passe par la prise de conscience des schémas qui nous enferment et dont nous pouvons nous défaire pour mieux vivre et dans cette libération toucher l'essence du vrai et la conscience universelle, celle du tout réunit.
 
 Ainsi, les constellations portent en elle un message que notre Soleil nous transmet depuis le fond des âges avec toute l'abondance que nous lui connaissons. Chaque mois, les rayons solaires agissent comme des messagers et déversent sur la Terre de très nombreux signes observables à ceux qui sont aguerris à voir au-delà des formes et travaillent à se libérer des biais cognitifs. Chaque moi, une tonalité particulière fait vibrer les atomes de matière. Cette énergie magnétise notre énergie vitale, émotionnelle, mentale et spirituelle dans une certaine direction, obligeant à faire face à des défis tant individuels que collectifs. Cependant, elles offrent aussi des ressources nouvelles, lumineuses et inspirantes poussant chaque individu vers l'éveil ou au réveille de l'âme. Par la recherche de l'équilibre à travers le corps, nous accédons au cœur. Par le cœur nous accédons à l'âme, le point d'unité en soi. Ceci est l'ascension ardente de l'Amour. L'ouverture du cœur permet à l'Amour de se déverser depuis l'âme et de creuser la voie de la somptueuse Lumière et d'enflammer l'existence. La conscience qui s'élargit est une lumière qui se répand et éclaire tant la pensée que le geste. Le Sublime se fait parure du Feu éternel et incandescent. Il allume chaque étincelle de vie et lui rappelle sa glorieuse et triomphante destinée. Cette progression est observable par les quatre marqueurs de l'Unité.`
     },
     {
         id: 12,
-        title: "La Réunion - L'Union - L'Unité - La Synthèse",
+        title: "L'Unité",
         content: `La Réunion - L'Union - L'Unité - La Synthèse
 
 Ces quatre points de réalisation façonnent la transcendance de la personnalité et l'immanence du cœur. La réalisation de chaque être humain à travers ces quatre points de réalisation, participe pleinement à l'évolution et l'accomplissement de la Terre.
